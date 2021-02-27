db52876114bee70fbd184e20100a4d4ccb446e5a
Bruno Moreno <brunomorello7@gmail.com>
2021-02-27 09:46:07 -0300

    Initialize Repository - taskmanager v2 with Spring

---
+++ README.md
@@ -0,0 +1,1 @@
+# taskmanagerv2
---
+++ src/main/java/br/com/bmo/taskmanager/Taskmanagerv2Application.java
@@ -0,0 +1,13 @@
+package br.com.bmo.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskmanagerv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskmanagerv2Application.class, args);
+	}
+
+}
