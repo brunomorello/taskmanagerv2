6cddbc1009b28ddbddcb643447103bdf72e47503
Bruno Moreno <brunomorello7@gmail.com>
2021-02-28 18:05:55 -0300

    Using Derived Queries

--- src/main/java/br/com/bmo/taskmanager/repository/UserRepository.java
+++ src/main/java/br/com/bmo/taskmanager/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package br.com.bmo.taskmanager.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import br.com.bmo.taskmanager.orm.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer>{
+	List<User> findByFirstNameLike(String firstName);
 }
