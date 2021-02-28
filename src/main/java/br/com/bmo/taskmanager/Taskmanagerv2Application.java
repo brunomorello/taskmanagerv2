caf91464ae60def3f618e297f8f8a30c35983b41
Bruno Moreno <brunomorello7@gmail.com>
2021-02-28 10:41:36 -0300

    Category using CrudRepository and testing save method

--- src/main/java/br/com/bmo/taskmanager/Taskmanagerv2Application.java
+++ src/main/java/br/com/bmo/taskmanager/Taskmanagerv2Application.java
@@ -1,13 +1,31 @@
 package br.com.bmo.taskmanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.bmo.taskmanager.orm.Category;
+import br.com.bmo.taskmanager.repository.CategoryRepository;
+
 @SpringBootApplication
-public class Taskmanagerv2Application {
+public class Taskmanagerv2Application implements CommandLineRunner {
+
+	private final CategoryRepository repository;
+	
+	public Taskmanagerv2Application(CategoryRepository repository) {
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Taskmanagerv2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category category = new Category();
+		category.setName("Housekeeping");
+		
+		repository.save(category);
 	}
 
 }
---
+++ src/main/java/br/com/bmo/taskmanager/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package br.com.bmo.taskmanager.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.bmo.taskmanager.orm.Category;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+}
