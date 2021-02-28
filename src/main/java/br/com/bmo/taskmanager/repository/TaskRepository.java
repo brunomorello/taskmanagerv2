df9cb12818caaf91cdedd773d22772811be769f4
Bruno Moreno <brunomorello7@gmail.com>
2021-02-28 18:06:16 -0300

    using JPQL

--- src/main/java/br/com/bmo/taskmanager/repository/TaskRepository.java
+++ src/main/java/br/com/bmo/taskmanager/repository/TaskRepository.java
@@ -1,5 +1,8 @@
 package br.com.bmo.taskmanager.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +10,6 @@ import br.com.bmo.taskmanager.orm.Task;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Integer>{
+	@Query("SELECT t from Task t WHERE month(t.createdAt) = :month")
+	List<Task> findTasksByMonth(Integer month);
 }
