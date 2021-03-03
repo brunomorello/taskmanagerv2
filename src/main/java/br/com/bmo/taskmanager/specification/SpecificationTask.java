03ce895f3cbc3b368682a107213bc42d6fb68311
Bruno Moreno <brunomorello7@gmail.com>
2021-03-02 22:22:51 -0300

    Created Specification Task

---
+++ src/main/java/br/com/bmo/taskmanager/specification/SpecificationTask.java
@@ -0,0 +1,35 @@
+package br.com.bmo.taskmanager.specification;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import br.com.bmo.taskmanager.orm.Task;
+
+public class SpecificationTask {
+
+	public static Specification<Task> description(String description) {
+		return (root, criteriaQuery, criteriaBuilder) ->
+			criteriaBuilder.like(root.get("description"), "%" + description + "%");
+	}
+	
+	public static Specification<Task> dueDate(LocalDateTime dateTime) {
+		return (root, criteriaQuery, criteriaBuilder) ->
+		criteriaBuilder.greaterThan(root.get("dueDate"), dateTime);
+	}
+	
+	public static Specification<Task> category(Integer categoryId) {
+		return (root, criteriaQuery, criteriaBuilder) ->
+			criteriaBuilder.equal(root.get("category"), categoryId);
+	}
+	
+	public static Specification<Task> status(Integer statusId) {
+		return (root, criteriaQuery, criteriaBuilder) ->
+			criteriaBuilder.equal(root.get("status"), statusId);
+	}
+	
+	public static Specification<Task> owner(Integer ownerId) {
+		return (root, criteriaQuery, criteriaBuilder) ->
+			criteriaBuilder.equal(root.get("owner"), ownerId);
+	}
+}
