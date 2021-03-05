866a97f44139477900cd4477630f40b4f74e983a
Bruno Moreno <brunomorello7@gmail.com>
2021-03-05 08:22:57 -0300

    using autoriwed

--- src/main/java/br/com/bmo/taskmanager/controller/HomeController.java
+++ src/main/java/br/com/bmo/taskmanager/controller/HomeController.java
@@ -2,6 +2,7 @@ package br.com.bmo.taskmanager.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,20 +17,31 @@ import br.com.bmo.taskmanager.service.UserService;
 @Controller
 public class HomeController {
 	
-	private final CategoryService categoryService;
-	private final UserService userService;
-	private final StatusService statusService;
-	private final TaskService taskService;
-	private final TaskReportService taskReportService;
+	@Autowired
+	private CategoryService categoryService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private StatusService statusService;
+	@Autowired
+	private TaskService taskService;
+	@Autowired
+	private TaskReportService taskReportService;
 	
-	public HomeController(CategoryService categoryService, UserService userService, StatusService statusService,
-			TaskService taskService, TaskReportService taskReportService) {
-		this.categoryService = categoryService;
-		this.userService = userService;
-		this.statusService = statusService;
-		this.taskService = taskService;
-		this.taskReportService = taskReportService;
-	}
+//	private final CategoryService categoryService;
+//	private final UserService userService;
+//	private final StatusService statusService;
+//	private final TaskService taskService;
+//	private final TaskReportService taskReportService;
+	
+//	public HomeController(CategoryService categoryService, UserService userService, StatusService statusService,
+//			TaskService taskService, TaskReportService taskReportService) {
+//		this.categoryService = categoryService;
+//		this.userService = userService;
+//		this.statusService = statusService;
+//		this.taskService = taskService;
+//		this.taskReportService = taskReportService;
+//	}
 
 	@GetMapping("/home")
 	public String home(Model model) {
