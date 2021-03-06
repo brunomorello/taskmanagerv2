3ac961ff53f84eac22eba1958bc02da0e1097d4e
Bruno Moreno <brunomorello7@gmail.com>
2021-03-06 15:20:51 -0300

    home, form and new task actions created

--- src/main/java/br/com/bmo/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/bmo/taskmanager/controller/TaskController.java
@@ -1,13 +1,24 @@
 package br.com.bmo.taskmanager.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.bmo.taskmanager.dto.RequestNewTask;
 import br.com.bmo.taskmanager.model.Category;
+import br.com.bmo.taskmanager.model.Status;
+import br.com.bmo.taskmanager.model.Task;
+import br.com.bmo.taskmanager.model.User;
 import br.com.bmo.taskmanager.repository.CategoryRepository;
+import br.com.bmo.taskmanager.repository.StatusRepository;
+import br.com.bmo.taskmanager.repository.TaskRepository;
+import br.com.bmo.taskmanager.repository.UserRepository;
+import br.com.bmo.taskmanager.service.TaskService;
 
 @Controller
 @RequestMapping("task")
@@ -15,19 +26,40 @@ public class TaskController {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private TaskRepository taskRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private StatusRepository statusRepository; 
+	@Autowired
+	private TaskService taskService;
+	
+	@GetMapping("/home")
+	public ModelAndView home() {
+		List<Task> allTasksByCategory = taskService.getTasksByCategoryNameLike("%study%");
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("tasks", allTasksByCategory);
+		return mv;
+	}
 
 	@GetMapping("form")
 	public ModelAndView form() {
 		ModelAndView mv = new ModelAndView("task/form");
 		Iterable<Category> categories = categoryRepository.findAll();
 		mv.addObject("categories", categories);
+		Iterable<User> users = userRepository.findAll();
+		mv.addObject("users", users);
+		Iterable<Status> statusList = statusRepository.findAll();
+		mv.addObject("statusList", statusList);
 		return mv;
 	}
 	
-	@GetMapping("new")
+	@PostMapping("new")
 	public ModelAndView newTask(RequestNewTask request) {
-		ModelAndView mv = new ModelAndView("home");
-		request.toTask();
+		ModelAndView mv = new ModelAndView("/home");
+		Task task = request.toTask();
+		taskRepository.save(task);
 		return mv;
 	}
 }
