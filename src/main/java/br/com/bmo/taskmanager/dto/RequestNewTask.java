bb83bd3434a9515f93ac701cf51009eaed2e418c
Bruno Moreno <brunomorello7@gmail.com>
2021-03-06 15:21:06 -0300

    DTO for Task Object

--- src/main/java/br/com/bmo/taskmanager/dto/RequestNewTask.java
+++ src/main/java/br/com/bmo/taskmanager/dto/RequestNewTask.java
@@ -1,5 +1,82 @@
 package br.com.bmo.taskmanager.dto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import br.com.bmo.taskmanager.model.Category;
+import br.com.bmo.taskmanager.model.Status;
+import br.com.bmo.taskmanager.model.Task;
+import br.com.bmo.taskmanager.model.User;
+
 public class RequestNewTask {
 
+	private String taskDesc;
+	private String taskCategory;
+	private String taskDueDate;
+	private String taskDetails;
+	private String taskOwner;
+	private String taskStatus;
+	
+	public String getTaskStatus() {
+		return taskStatus;
+	}
+	public void setTaskStatus(String taskStatus) {
+		this.taskStatus = taskStatus;
+	}
+	public String getTaskDesc() {
+		return taskDesc;
+	}
+	public void setTaskDesc(String taskDesc) {
+		this.taskDesc = taskDesc;
+	}
+	public String getTaskCategory() {
+		return taskCategory;
+	}
+	public void setTaskCategory(String taskCategory) {
+		this.taskCategory = taskCategory;
+	}
+	public String getTaskDueDate() {
+		return taskDueDate;
+	}
+	public void setTaskDueDate(String taskDueDate) {
+		this.taskDueDate = taskDueDate;
+	}
+	public String getTaskDetails() {
+		return taskDetails;
+	}
+	public void setTaskDetails(String taskDetails) {
+		this.taskDetails = taskDetails;
+	}
+	public String getTaskOwner() {
+		return taskOwner;
+	}
+	public void setTaskOwner(String taskOwner) {
+		this.taskOwner = taskOwner;
+	}
+	
+	public Task toTask() {
+		Task task = new Task();
+		task.setDescription(getTaskDesc());
+		
+		Category category = new Category();
+		category.setId(Integer.valueOf(getTaskCategory()));
+		task.setCategory(category);
+		
+		User user = new User();
+		user.setId(Integer.valueOf(getTaskOwner()));
+		task.setOwner(user);
+		
+		Status status = new Status();
+		status.setId(Integer.valueOf(getTaskStatus()));
+		task.setStatus(status);
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		LocalDateTime dueDate = LocalDateTime.parse(taskDueDate, formatter);
+		task.setDueDate(dueDate);
+		
+		task.setDetails(getTaskDetails());
+		return task;
+	}
+	
+	
 }
