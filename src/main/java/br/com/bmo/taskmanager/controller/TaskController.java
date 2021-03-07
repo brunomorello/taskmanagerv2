6a69cb1af1751f321a5ce34e83a0fd11157ae678
Bruno Moreno <brunomorello7@gmail.com>
2021-03-07 13:33:46 -0300

    onError treatment

--- src/main/java/br/com/bmo/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/bmo/taskmanager/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,5 +78,10 @@ public class TaskController {
 		model.addAttribute("tasks", tasks);
 		model.addAttribute("status", status);
 		return "home";
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/task/home";
 	}
 }
