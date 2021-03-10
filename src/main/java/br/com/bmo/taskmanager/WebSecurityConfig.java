b9cd8876e1ae27caaee90db5eec3b4a97c3314ab
Bruno Moreno <brunomorello7@gmail.com>
2021-03-10 19:27:18 -0300

    implemented JDBC Auth

--- src/main/java/br/com/bmo/taskmanager/WebSecurityConfig.java
+++ src/main/java/br/com/bmo/taskmanager/WebSecurityConfig.java
@@ -1,39 +1,47 @@
 package br.com.bmo.taskmanager;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
-			.formLogin(form -> form
+				.formLogin(form -> form
 					.loginPage("/login")
+					.defaultSuccessUrl("/home", true)
 					.permitAll()
-			)
-			.logout(logout -> logout.logoutUrl("/logout"));
+				)
+				.logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")));
 	}
 	
-	@Bean
 	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user = User.withDefaultPasswordEncoder()
-			.username("bmoreno")
-			.password("123")
-			.roles("ADM")
-			.build();
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.passwordEncoder(encoder);
 	}
 }
