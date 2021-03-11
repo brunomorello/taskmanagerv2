dada40a28570077103d02a637c9a13acbe6ada4f
Bruno Moreno <brunomorello7@gmail.com>
2021-03-11 08:52:36 -0300

    removed relationship starting by Category

--- src/main/java/br/com/bmo/taskmanager/model/Category.java
+++ src/main/java/br/com/bmo/taskmanager/model/Category.java
@@ -1,12 +1,9 @@
 package br.com.bmo.taskmanager.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,8 +14,6 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	@OneToMany(mappedBy = "category")
-	private List<Task> task;
 		
 	public Category() {
 	}
@@ -37,14 +32,6 @@ public class Category {
 	
 	public void setName(String name) {
 		this.name = name;
-	}
-	
-	public List<Task> getTask() {
-		return task;
-	}
-
-	public void setTask(List<Task> task) {
-		this.task = task;
 	}
 
 	@Override
