b05c06845534db11fec48788c6cf6f85cb9f5d53
Bruno Moreno <brunomorello7@gmail.com>
2021-03-27 15:46:39 -0300

    Implemented a Create Task endpoint

--- src/main/java/br/com/bmo/taskmanager/controller/api/TaskAPIController.java
+++ src/main/java/br/com/bmo/taskmanager/controller/api/TaskAPIController.java
@@ -5,11 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bmo.taskmanager.controller.dto.TaskDTO;
+import br.com.bmo.taskmanager.controller.dto.TaskDTOForm;
+import br.com.bmo.taskmanager.model.Task;
+import br.com.bmo.taskmanager.repository.CategoryRepository;
+import br.com.bmo.taskmanager.repository.StatusRepository;
 import br.com.bmo.taskmanager.repository.TaskRepository;
+import br.com.bmo.taskmanager.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -17,6 +24,12 @@ public class TaskAPIController {
 
 	@Autowired
 	private TaskRepository taskRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private StatusRepository statusRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@GetMapping("/")
 	public List<TaskDTO> getAllTasks(String username) {
@@ -24,5 +37,11 @@ public class TaskAPIController {
 			return TaskDTO.toList(taskRepository.findAll(Sort.by("createdAt").ascending()));
 		else
 			return TaskDTO.toList(taskRepository.findByOwner_Username(username));
+	}
+	
+	@PostMapping("/")
+	public void createTask(@RequestBody TaskDTOForm form) {
+		Task task = form.parse(userRepository, statusRepository, categoryRepository);
+		taskRepository.save(task);
 	}
 }
---
+++ src/main/java/br/com/bmo/taskmanager/controller/dto/TaskDTOForm.java
@@ -0,0 +1,73 @@
+package br.com.bmo.taskmanager.controller.dto;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import br.com.bmo.taskmanager.model.Category;
+import br.com.bmo.taskmanager.model.Status;
+import br.com.bmo.taskmanager.model.Task;
+import br.com.bmo.taskmanager.model.User;
+import br.com.bmo.taskmanager.repository.CategoryRepository;
+import br.com.bmo.taskmanager.repository.StatusRepository;
+import br.com.bmo.taskmanager.repository.UserRepository;
+
+public class TaskDTOForm {
+
+	private String description;
+	private String details;
+	private String category;
+	private String status;
+	private String owner;
+	private String dueDate;
+	
+	public TaskDTOForm() {
+		
+	}
+	
+	public TaskDTOForm(String description, String details, String category, String status, String owner, String dueDate) {
+		this.description = description;
+		this.details = details;
+		this.category = category;
+		this.status = status;
+		this.owner = owner;
+		this.dueDate = dueDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+	
+	public String getDueDate() {
+		return dueDate;
+	}
+	
+	public Task parse(UserRepository userRepository, StatusRepository statusRepository, CategoryRepository categoryRepository) {
+		User userObj = userRepository.findByUsername(getOwner());
+		Status statusObj = statusRepository.findByName(getStatus());
+		Category categoryObj = categoryRepository.findByName(getCategory());
+		return new Task(
+				getDescription(), 
+				LocalDateTime.parse(getDueDate(), DateTimeFormatter.ISO_LOCAL_DATE_TIME), 
+				userObj, 
+				statusObj, 
+				categoryObj, 
+				getDetails());
+	}
+	
+}
