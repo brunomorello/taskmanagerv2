94c6de80a115b45c3c452f707b0c31749fdb79c7
Bruno Moreno <brunomorello7@gmail.com>
2021-03-28 12:32:51 -0300

    implemented get task by id

--- src/main/java/br/com/bmo/taskmanager/controller/api/TaskAPIController.java
+++ src/main/java/br/com/bmo/taskmanager/controller/api/TaskAPIController.java
@@ -2,6 +2,7 @@ package br.com.bmo.taskmanager.controller.api;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,5 +65,21 @@ public class TaskAPIController {
 						task.getUpdatedAt() != null ? task.getUpdatedAt().toString() : ""
 					)
 				);
+	}
+	
+	@GetMapping("/{id}")
+	public TaskDTO taskDetails(@PathVariable Integer id) {
+		Optional<Task> taskOpt = taskRepository.findById(id);
+		Task task = taskOpt.get();
+		return new TaskDTO(
+				task.getId(), 
+				task.getDescription(), 
+				task.getDetails(), 
+				task.getStatus(), 
+				task.getCategory(), 
+				task.getOwner().getUsername(), 
+				task.getCreatedAt().toString(), 
+				task.getUpdatedAt() != null ? task.getUpdatedAt().toString() : ""
+			);
 	}
 }
