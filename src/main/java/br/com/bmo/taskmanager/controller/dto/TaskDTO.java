67b79a9b174105948640fc4b53093907fc64cf5e
Bruno Moreno <brunomorello7@gmail.com>
2021-03-29 08:24:34 -0300

    Improved TaskDTO. Added override of toString, hashCode and equals Created new Consutructor to receive a Task Object Updated toList method to receive a Page<Task> and use java 8 map

--- src/main/java/br/com/bmo/taskmanager/controller/dto/TaskDTO.java
+++ src/main/java/br/com/bmo/taskmanager/controller/dto/TaskDTO.java
@@ -1,8 +1,6 @@
 package br.com.bmo.taskmanager.controller.dto;
 
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 import br.com.bmo.taskmanager.model.Category;
 import br.com.bmo.taskmanager.model.Status;
@@ -30,6 +28,18 @@ public class TaskDTO {
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
+	
+	public TaskDTO(Task task) {
+		this.id = task.getId();
+		this.description = task.getDescription();
+		this.details = task.getDetails();
+		this.status = task.getStatus();
+		this.category = task.getCategory();
+		this.owner = task.getOwner().getUsername();
+		this.createdAt = task.getCreatedAt().toString();
+		this.updatedAt = task.getUpdatedAt().toString();
+	}
+	
 	
 	public Integer getId() {
 		return id;
@@ -63,26 +73,100 @@ public class TaskDTO {
 		return updatedAt;
 	}
 
-	public static List<TaskDTO> toList(List<Task> findAll) {
-		List<TaskDTO> parsedTasksList = new ArrayList<>();
-		findAll.forEach(task -> {
-			parsedTasksList.add(
-					new TaskDTO(
-						task.getId(), 
-						task.getDescription(), 
-						task.getDetails(), 
-						task.getStatus(), 
-						task.getCategory(), 
-						task.getOwner().getUsername(), 
-						task.getCreatedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), 
-						task.getUpdatedAt() != null ? task.getUpdatedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : "")
-					);
-		});
+	public static Page<TaskDTO> toList(Page<Task> allTasks) {
+//		List<TaskDTO> parsedTasksList = new ArrayList<>();
+//		findAll.forEach(task -> {
+//			parsedTasksList.add(
+//					new TaskDTO(
+//						task.getId(), 
+//						task.getDescription(), 
+//						task.getDetails(), 
+//						task.getStatus(), 
+//						task.getCategory(), 
+//						task.getOwner().getUsername(), 
+//						task.getCreatedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME), 
+//						task.getUpdatedAt() != null ? task.getUpdatedAt().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) : "")
+//					);
+//		});
 		// Trying java 8 - no success
 //		List<Task> result = StreamSupport.stream(findAll.spliterator(), false)
 //				.collect(Collectors.toList());
 //		return result.stream().map(TaskDTO::new).collect(Collectors.toList());
-		return parsedTasksList;
+		return allTasks.map(TaskDTO::new);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((details == null) ? 0 : details.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaskDTO other = (TaskDTO) obj;
+		if (category == null) {
+			if (other.category != null)
+				return false;
+		} else if (!category.equals(other.category))
+			return false;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (details == null) {
+			if (other.details != null)
+				return false;
+		} else if (!details.equals(other.details))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (owner == null) {
+			if (other.owner != null)
+				return false;
+		} else if (!owner.equals(other.owner))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (updatedAt == null) {
+			if (other.updatedAt != null)
+				return false;
+		} else if (!updatedAt.equals(other.updatedAt))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "TaskDTO [id=" + id + ", description=" + description + ", details=" + details + ", status=" + status
+				+ ", category=" + category + ", owner=" + owner + ", createdAt=" + createdAt + ", updatedAt="
+				+ updatedAt + "]";
+	}
+	
 }
