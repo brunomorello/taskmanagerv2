0c6c5a06429fdfde1e68b46926bd1e364c41fc0c
Bruno Moreno <brunomorello7@gmail.com>
2021-03-31 08:07:16 -0300

    Moved WebSecurity to its package and granted public access do list tasks and categories through API

--- src/main/java/br/com/bmo/taskmanager/config/security/WebSecurityConfig.java
+++ src/main/java/br/com/bmo/taskmanager/config/security/WebSecurityConfig.java
@@ -1,11 +1,13 @@
-package br.com.bmo.taskmanager;
+package br.com.bmo.taskmanager.config.security;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,10 +20,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private DataSource dataSource;
 
+	// Authorization Configs
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.anyRequest().authenticated()
+			.antMatchers(HttpMethod.GET, "/api/tasks/").permitAll()
+			.antMatchers(HttpMethod.GET, "/api/tasks/*").permitAll()
+			.antMatchers(HttpMethod.GET, "/api/categories/").permitAll()
+			.antMatchers(HttpMethod.GET, "/api/categories/*").permitAll()
+			.anyRequest().authenticated()
 			.and()
 				.formLogin(form -> form
 					.loginPage("/login")
@@ -35,6 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.csrf().disable();
 	}
 	
+	// Authentication Configs
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -42,5 +50,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.jdbcAuthentication()
 			.dataSource(dataSource)
 			.passwordEncoder(encoder);
+	}
+
+	// Statict Resources Configs
+	@Override
+	public void configure(WebSecurity web) throws Exception {
 	}
 }
