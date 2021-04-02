e9ff822097ff9b4ed13beebcdb28cf777a9fd9eb
Bruno Moreno <brunomorello7@gmail.com>
2021-04-02 10:35:24 -0300

    Implemented a Custom Authentication Filter to intercepet JWT and valid it

---
+++ src/main/java/br/com/bmo/taskmanager/config/security/AuthenticationUsingTokenFilter.java
@@ -0,0 +1,39 @@
+package br.com.bmo.taskmanager.config.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AuthenticationUsingTokenFilter extends OncePerRequestFilter {
+	
+	private TokenService tokenService;
+	
+	public AuthenticationUsingTokenFilter(TokenService tokenService) {
+		this.tokenService = tokenService;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String token = getToken(request);
+		boolean valid = tokenService.isValid(token);
+		System.out.println(valid);
+		filterChain.doFilter(request, response);
+	}
+
+	private String getToken(HttpServletRequest request) {
+		String token = request.getHeader("Authorization");
+		if (token == null || token.isEmpty() || ! token.startsWith("Bearer ")) {
+			return null;
+		}
+		return token.substring(7, token.length());
+	}
+
+
+}
