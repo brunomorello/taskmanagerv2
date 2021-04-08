39dfc88c931831ffaea5f2decee1ff5ab7e70ef7
Bruno Moreno <brunomorello7@gmail.com>
2021-04-07 21:13:34 -0300

    removed custom interceptors to fix swagger ui

--- src/main/java/br/com/bmo/taskmanager/Taskmanagerv2Application.java
+++ src/main/java/br/com/bmo/taskmanager/Taskmanagerv2Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import br.com.bmo.taskmanager.model.Category;
 import br.com.bmo.taskmanager.model.Status;
@@ -19,12 +20,10 @@ import br.com.bmo.taskmanager.service.StatusService;
 import br.com.bmo.taskmanager.service.TaskReportService;
 import br.com.bmo.taskmanager.service.TaskService;
 import br.com.bmo.taskmanager.service.UserService;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
 @EnableSpringDataWebSupport
-@EnableSwagger2
 public class Taskmanagerv2Application implements CommandLineRunner {
 	
 //	private final CategoryService categoryService;
